d80d8bd8379c55161ec290ce403878cecc09ee7d
Shamair <shamair@live.com>
2023-11-14 20:37:47 -0500

    rip in peace my brain

--- app.py
+++ app.py
@@ -1,67 +1,64 @@
-
 from flask import Flask, request, jsonify, render_template
 import openai
-import time
 
 app = Flask(__name__)
 
-# Use the actual OpenAI API key
-client = openai.Client(api_key='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')
+# Configure your OpenAI API key
+#openai.api_key = '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'
 
-# The original code for file upload and assistant creation
-file = client.files.create(file=open("static/billionaire.txt", "rb"), purpose='assistants')
+# Initialize the OpenAI client for the Assistants API
+client = openai.Client(api_key='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')
 assistant = client.beta.assistants.create(
-    name="CAM-Lite",
-    instructions = "AI assistant that deals in business related inquiries for all levels of an organization",
-    tools = [{"type": "retrieval"}],
-    file_ids=[file.id],
+    name="Math Tutor",
+    instructions="You answer questions",
+    tools=[{"type": "code_interpreter"}],
     model="gpt-4-1106-preview"
-    )
-thread = client.beta.threads.create()
+)
 
 @app.route('/')
 def home():
     return render_template('chat.html')
 
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    global thread
-    user_input = request.data.decode('utf-8')
-    print(user_input)
-    #user_input = request.g['message']
-    message = client.beta.threads.messages.create(
-            thread_id=thread.id,
-            role="user",
-            content=user_input,
-            file_ids=[file.id],
-        )
+@app.route('/message', methods=['POST'])
+def message():
+    user_message = request.data.decode('utf-8')
+    #user_message = data['message']
+
+    # Create a Thread for the conversation
+    thread = client.beta.threads.create()
+
+    # Add the user's message to the Thread
+    client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=user_message
+    )
+
+    # Run the Assistant
     run = client.beta.threads.runs.create(
-            thread_id=thread.id,
-            assistant_id=assistant.id
+        thread_id=thread.id,
+        assistant_id=assistant.id
     )
-    
-    while True:
-        run_status = client.beta.threads.runs.retrieve(
-            thread_id=thread.id,
-            run_id=run.id
-        )
 
-        if run_status.completed_at is not None:
-            break
-        
-    messages = client.beta.threads.messages.list(
-             thread_id=thread.id
+    # Retrieve the latest messages from the Thread
+    response_messages = client.beta.threads.messages.list(
+        thread_id=thread.id,
     )
-    
-    for msg in messages.data:
-             if msg.role == 'assistant':
-                  for content in msg.content:
-                       if content.type == 'text':
-                            assistant_response = content.text.value
+    print(response_messages)
 
-    print(content.text.value)
-    return jsonify({'response': assistant_response})
+    assistant_response = ' '
+    #for message in response_messages:
+    #    assistant_response = message
+    #    break
+    for msg in response_messages.data:
+        for content in msg.content:
+            if content.type == 'text':
+                assistant_response = content.text.value
+    
+    reply = assistant_response if assistant_response else "No reply from the assistant."
+    print(reply)
+    return jsonify({"reply": reply})
+    #return reply
 
 if __name__ == '__main__':
-    app.debug=True
     app.run(debug=True)
---
+++ app_current.py
@@ -0,0 +1,66 @@
+
+from flask import Flask, request, jsonify, render_template
+import openai
+import time
+
+app = Flask(__name__)
+
+# Use the actual OpenAI API key
+client = openai.Client(api_key='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')
+
+# The original code for file upload and assistant creation
+file = client.files.create(file=open("static/billionaire.txt", "rb"), purpose='assistants')
+assistant = client.beta.assistants.create(
+    name="CAM-Lite",
+    instructions = "AI assistant that deals in business related inquiries for all levels of an organization",
+    tools = [{"type": "retrieval"}],
+    file_ids=[file.id],
+    model="gpt-4-1106-preview"
+    )
+thread = client.beta.threads.create()
+
+@app.route('/')
+def home():
+    return render_template('chat.html')
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    user_input = request.data.decode('utf-8')
+    print(user_input)
+    #user_input = request.g['message']
+    message = client.beta.threads.messages.create(
+            thread_id=thread.id,
+            role="user",
+            content=user_input,
+            file_ids=[file.id],
+        )
+    run = client.beta.threads.runs.create(
+            thread_id=thread.id,
+            assistant_id=assistant.id
+    )
+    
+    while True:
+        run_status = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id
+        )
+
+        if run_status.completed_at is not None:
+            break
+        
+    messages = client.beta.threads.messages.list(
+             thread_id=thread.id
+    )
+    
+    for msg in messages.data:
+             if msg.role == 'assistant':
+                  for content in msg.content:
+                       if content.type == 'text':
+                            assistant_response = content.text.value
+
+    print(content.text.value)
+    return jsonify({'response': assistant_response})
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run(debug=True)
