eedf96ed05948b42978e75abec9bb0d92f017053
Shamair <shamair@live.com>
2023-11-17 13:45:07 -0500

    ui fixes, added stylesheet, dynamic scaling

--- app.py
+++ app.py
@@ -9,30 +9,32 @@ app = Flask(__name__)
 client = openai.Client(api_key='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')
 
 # The original code for file upload and assistant creation
-file = client.files.create(file=open("static/billionaire.txt", "rb"), purpose='assistants')
+#file = client.files.create(file=open("static/billionaire.txt", "rb"), purpose='assistants')
 assistant = client.beta.assistants.create(
     name="CAM-Lite",
     instructions = "AI assistant that deals in business related inquiries for all levels of an organization",
-    tools = [{"type": "retrieval"}],
-    file_ids=[file.id],
+    #tools = [{"type": "retrieval"}],
+    tools = [],
+    #file_ids=[file.id],
     model="gpt-4-1106-preview"
     )
 thread = client.beta.threads.create()
 
 @app.route('/')
 def home():
-    return render_template('chat.html')
+    return render_template('chat_modified.html')
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
     user_input = request.data.decode('utf-8')
     print(user_input)
     #user_input = request.g['message']
+    #thread = client.beta.threads.create()
     messages = client.beta.threads.messages.create(
             thread_id=thread.id,
             role="user",
             content=user_input,
-            file_ids=[file.id],
+            #file_ids=[file.id],
         )
     run = client.beta.threads.runs.create(
             thread_id=thread.id,
@@ -49,14 +51,15 @@ def send_message():
             break
         
     messages = client.beta.threads.messages.list(
-             thread_id=thread.id
+             thread_id=thread.id,
+             order='asc'
     )
     print('\nresponse message from threads: \n:' + str(messages))
     for msg in messages.data:
-             if msg.role == 'assistant':
-                  for content in msg.content:
-                       if content.type == 'text':
-                            assistant_response = content.text.value
+                if msg.role == 'assistant':
+                    for content in msg.content:
+                        if content.type == 'text':
+                                assistant_response = content.text.value
 
     print('\nassistant output: ' + str(assistant_response) + '\n')
     return jsonify({'response': assistant_response})
